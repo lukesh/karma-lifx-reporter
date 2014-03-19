c32d5cfb080386181633f4bd02a8c417610d1c56
Francis Lukesh <fran@rentlytics.com>
2014-03-19 10:36:01 -0700

    Re-initialize LIFX connection when tests run.

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ lifx = require('lifx')
 
 var LifxReporter = function(baseReporterDecorator) {
   var lx
+  lifx.setDebug = false;
 
 	baseReporterDecorator(this);
 
-  lifx.setDebug = false;
-
-  lx = lifx.init();
-
 	this.onSpecComplete = function() { };
 
-    this.onRunComplete = function(browsers, results) {
-      lx.lightsOn();
+  this.onRunComplete = function(browsers, results) {
+    lx = lifx.init();
+    lx.lightsOn();
 
 		if (results.failed || results.error) {
       lx.lightsColour(0xFFFF, 0xFFFF, 1000, 0, 0);
