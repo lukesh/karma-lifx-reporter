bb9fd444fc25540cb10a4a6b2a745b4eeb18d104
Francis Lukesh <fran@rentlytics.com>
2014-03-19 14:00:12 -0700

    More intelligent management of connection.

--- index.js
+++ index.js
@@ -1,26 +1,73 @@
-lifx = require('lifx')
+var lifx = require('lifx');
+var lx = lifx.init();
+lifx.setDebug = false;
 
 var LifxReporter = function(baseReporterDecorator) {
-  var lx
-  lifx.setDebug = false;
-  lx = lifx.init();
-
 	baseReporterDecorator(this);
 
-  setInterval(function() {
+  var isConnected = true;
+  var timeouts = [];
+  var interval = 0;
+  var color = 0;
+
+  var turnLightOn = function() {
     lx.lightsOn();
-  }, 10000);
+  };
+
+  var turnLightOff = function() {
+    lx.lightsOff();
+  };
+
+  var setDimLight = function() {
+    lx.lightsColour(color, 0xFFFF, 800, 0, 0x0513);
+  };
+
+  var setBrightLight = function() {
+    lx.lightsColour(color, 0xFFFF, 1000, 0, 0x0513);
+  };
+
+  var setProcessingLight = function() {
+    lx.lightsColour(color, 0, 800, 0, 0x0513);
+  };
+
+  var openConnection = function() {
+    if(!isConnected) {
+      lx.gateways.forEach(function(gateway) {
+        gateway.connect();
+      });
+      isConnected = true;
+    }
+  };
+
+  var closeConnection = function() {
+    lx.close();
+    isConnected = false;
+  };
 
 	this.onSpecComplete = function() { };
 
-  this.onRunComplete = function(browsers, results) {
-    lx.lightsOn();
+  this.onRunStart = function() {
+    timeouts.forEach(function(val) {
+      clearTimeout(val);
+    });
+    openConnection();
+    turnLightOn();
+    //setProcessingLight();
+    this._browsers = [];
+    this.browserResults = {};
+  };
 
-		if (results.failed || results.error) {
-      lx.lightsColour(0xFFFF, 0xFFFF, 1000, 0, 0);
-		} else {
-      lx.lightsColour(0x3333, 0xFFFF, 1000, 0, 0);
+  this.onRunComplete = function(browsers, results) {
+    turnLightOn();
+    if (results.failed || results.error) {
+      color = 0xFFFF;
+      setBrightLight();
+    } else {
+      color = 0x3333;
+      setBrightLight();
     }
+    timeouts.push(setTimeout(setDimLight, 1000));
+    timeouts.push(setTimeout(closeConnection, 2000));
 	};
 };
 
